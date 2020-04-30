0e6b16fbcadc29ec64ae4f8943404bb0228845cd
Nathan Martel <nathanmartel@gmail.com>
2020-04-29 21:38:56 -0700

    App loaded

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import List from '../List/List';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [monsters, setMonsters] = useState([]);
+
+  useEffect(() => {
+    setMonsters([{
+      name: 'Lorem Ipsum',
+      image: 'https://placekitten.com/200/200',
+    }, {
+      name: 'Dolor Sitar',
+      image: 'https://placekitten.com/200/200',
+    }]);  
+  }, []);
+
+
+  return ( 
+    <>
+      <h1>X-Files Monsters of the Week</h1>
+      <List monsters={monsters} />
+    </>
+  );
 }
-  
